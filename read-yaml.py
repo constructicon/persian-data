0f64509a364ae9a106de28aed54b0e2e8f94f1ed
Reza Soltani <108338776+rezasoltani88@users.noreply.github.com>
2024-01-23 21:57:39 +0100

    removed  errors='ignore' from line 4

--- read-yaml.py
+++ read-yaml.py
@@ -1,7 +1,7 @@
 import yaml
 import pprint
 
-with open("0017.yml", "r", encoding="utf-8", errors='ignore') as f:
+with open("0017.yml", "r", encoding="utf-8") as f:
     try:
         data = yaml.safe_load(f)
     except yaml.YAMLError as e:
